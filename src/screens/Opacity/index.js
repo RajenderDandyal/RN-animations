25bb32bb7246484b5da2cf9b405d16c5bbd0095d
RajenderDandyal <39468888+RajenderDandyal@users.noreply.github.com>
2019-01-02 22:45:24 +0530

    opacity completed

--- src/screens/Opacity/index.js
+++ src/screens/Opacity/index.js
@@ -1,12 +1,34 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, {Component} from "react";
+import {Text, View, StyleSheet, Animated, TouchableNativeFeedback} from "react-native";
 
 class Opacity extends Component {
+  state = {
+    animation: new Animated.Value(1)
+  };
+  handleAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 0,
+      duration: 550,
+    }).start(()=>{
+      Animated.timing(this.state.animation,{
+        toValue:1,
+        duration:500
+      }).start()
+    })
+  };
+
   render() {
+    let animationStyles = {
+      opacity: this.state.animation
+    }
     return (
-      <View style={styles.container}>
-        <Text> Opacity </Text>
-      </View>
+        <View style={styles.container}>
+          <TouchableNativeFeedback onPress={this.handleAnimation}>
+            <Animated.View style={[{justifyContent: "center", alignItems: "center"}, styles.box, animationStyles]}>
+              <Text> Opacity </Text>
+            </Animated.View>
+          </TouchableNativeFeedback>
+        </View>
     );
   }
 }
@@ -17,5 +39,10 @@ let styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: "orange"
   }
 });
